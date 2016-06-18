f38a51bd1d2e299abf6fb2758ec5643f1535f0c9
corya0687 <corya0687@gmail.com>
2016-06-18 11:05:17 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   yellowLight();
   greenLight();
 
-  
+
 });
 
 function lightActive(){
@@ -16,21 +16,25 @@ function lightActive(){
 }
 
 function redLight(){
-  $(".redLight").on('click', function(){
+  $(".redLight").on('click', function(event){
     $(this).toggleClass("red");
+    event.stopPropagation();
   });
 }
 
 
 function yellowLight(){
-  $(".yellowLight").on('click', function(){
+  $(".yellowLight").on('click', function(event){
     $(this).toggleClass("yellow");
+    event.stopPropagation();
+
   });
 }
 
 function greenLight(){
-  $(".greenLight").on('click', function(){
+  $(".greenLight").on('click', function(event){
     $(this).toggleClass("green");
+    event.stopPropagation();
+
   });
 }
-
