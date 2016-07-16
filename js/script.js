576343badd81eae84459e1c9ad81964f88a12810
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-15 23:47:26 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -4,33 +4,35 @@ $(document).ready(function() {
   redLight();
   yellowLight();
   greenLight();
-
   
 });
 
 function lightActive(){
   $('.light').on('click', function(){
       $(this).toggleClass("active");
-      console.log("background purple")
+      console.log("background purple");
   });
 }
 
 function redLight(){
-  $(".redLight").on('click', function(){
+  $(".redLight").on('click', function(event){
     $(this).toggleClass("red");
+    event.stopPropagation();
   });
 }
 
 
 function yellowLight(){
-  $(".yellowLight").on('click', function(){
+  $(".yellowLight").on('click', function(event){
     $(this).toggleClass("yellow");
+    event.stopPropagation();
   });
 }
 
 function greenLight(){
-  $(".greenLight").on('click', function(){
+  $(".greenLight").on('click', function(event){
     $(this).toggleClass("green");
+    event.stopPropagation();
   });
 }
 
